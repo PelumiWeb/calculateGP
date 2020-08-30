015c0d38df1ca5bc8e9675ceb50a67e828c9973d
PelumiWeb <Pelumiogundipe905@gmail.com>
2020-08-29 23:16:02 -0700

    making some adjustments

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ const express = require("express");
 const app = express();
 const router = new express.Router();
 const port = process.env.PORT || 3030;
-const source = "./pelumi-calculategp/public";
-console.log(source);
-const test = app.use(express.static(source));
+// const source = "./pelumi-calculategp/public";
+// console.log(source);
+// const test = app.use(express.static(source));
+// app.set("view-engine", "ejs");
+
+// app.get("/", (req, res) => {
+//   res.render(".ejs");
+// });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build", "Index.html"));
+}
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
-
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static('client/build'));
-// }
-// app.get('*', (request, response) => {
-// 	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
