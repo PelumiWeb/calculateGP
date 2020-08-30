90c1d89609f4fb701dd11fdc179bbfe64cc8801a
PelumiWeb <Pelumiogundipe905@gmail.com>
2020-08-29 23:20:17 -0700

    making some adjustments

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const express = require("express");
 const app = express();
 const router = new express.Router();
 const port = process.env.PORT || 3030;
-// const source = "./pelumi-calculategp/public";
-// console.log(source);
-// const test = app.use(express.static(source));
+const source = "./pelumi-calculategp/public";
 // app.set("view-engine", "ejs");
 
 // app.get("/", (req, res) => {
 //   res.render(".ejs");
 // });
+app.use(express.static(source));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build", "Index.html"));
